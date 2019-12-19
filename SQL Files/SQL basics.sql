48e53b5190b7fe831d562eb63bae663bb24406a9
adnanshinwari <adnanshinwari70@gmail.com>
2019-12-19 23:13:25 +0500

    Fixed a minor error in use of paranthesis

--- SQL Files/SQL basics.sql
+++ SQL Files/SQL basics.sql
@@ -240,6 +240,5 @@ WHERE standard_qty = 0 AND (gloss_qty > 1000 OR poster_qty > 1000);
 
 SELECT *
 FROM accounts
-WHERE name LIKE '%C' OR name LIKE 'W%'
-	AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
-	AND primary_poc NOT LIKE '%eana%';
+WHERE (name LIKE 'C%' OR name LIKE 'W%') AND 
+      ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') AND primary_poc NOT LIKE '%eana%');
