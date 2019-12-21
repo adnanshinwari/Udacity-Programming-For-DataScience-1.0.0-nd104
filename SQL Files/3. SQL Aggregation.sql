e925dd109d2f450cd7efbbf0aa258a64170dc022
adnanshinwari <adnanshinwari70@gmail.com>
2019-12-21 23:20:45 +0500

    Update 3. SQL aggregation

--- SQL Files/3. SQL Aggregation.sql
+++ SQL Files/3. SQL Aggregation.sql
@@ -1,1 +1,12 @@
-/**/
+/*DATA TYPES AND NULL */
+SELECT *
+FROM accounts
+WHERE id >1500 AND id < 1600;
+
+SELECT *
+FROM accounts
+WHERE primary_poc IS NULL;
+
+SELECT *
+FROM accounts
+WHERE primary_poc IS NOT NULL;
