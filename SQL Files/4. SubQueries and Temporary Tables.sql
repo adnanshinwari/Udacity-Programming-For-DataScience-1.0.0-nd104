0f5c8b824ef726eac2ff1cee21abbe41f1f5eafa
adnanshinwari <adnanshinwari70@gmail.com>
2020-01-25 12:52:34 +0500

    Update 4. SubQueries and Temporary Tables.sql

--- SQL Files/4. SubQueries and Temporary Tables.sql
+++ SQL Files/4. SubQueries and Temporary Tables.sql
@@ -26,3 +26,31 @@ FROM (SELECT DATE_TRUNC('day', occurred_at) AS day, channel, COUNT(*) AS event_c
 	GROUP BY 1,2) sub
 GROUP BY 1
 ORDER BY 2 DESC;
+
+/* SubQuery Part II */
+SELECT MIN(occurred_at) AS min_time
+FROM orders;
+
+SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_month
+FROM orders;
+
+SELECT *
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = (SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_month
+											FROM orders)
+ORDER BY occurred_at;
+
+
+/* QUIZ: SubQuery Part II */
+SELECT DATE_TRUNC('month', MIN(occurred_at)) 
+FROM orders;
+
+SELECT AVG(standard_qty) AS avg_std_qty, AVG(poster_qty) AS avg_poster_qty, AVG(gloss_qty) AS AVG_gloss_qty
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = (SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_month
+											FROM orders);
+											
+SELECT SUM(total_amt_usd) AS amount_spent
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = (SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_month
+											FROM orders);
