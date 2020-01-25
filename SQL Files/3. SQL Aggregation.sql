f79e509a21bc0cfb7749835ad7a32e684abe49e1
adnanshinwari <adnanshinwari70@gmail.com>
2020-01-25 13:17:37 +0500

    Update 3. SQL Aggregation.sql

--- SQL Files/3. SQL Aggregation.sql
+++ SQL Files/3. SQL Aggregation.sql
@@ -458,3 +458,23 @@ JOIN sales_reps s
 ON s.id = a.sales_rep_id
 GROUP BY s.name
 ORDER BY 3 DESC;
+
+/* SUBQUERY Mania */
+SELECT t3.id, t3.name, t3.channel, t3.ct
+FROM (SELECT a.id, a.name, we.channel, COUNT(*) ct
+		FROM accounts a
+		JOIN web_events we
+		ON a.id = we.account_id
+		GROUP BY a.id, a.name, we.channel
+		ORDER BY a.id) t3
+JOIN (SELECT t1.id, t1.name, MAX(ct) max_chan
+		FROM(SELECT a.id, a.name, we.channel, COUNT(*) ct
+				FROM accounts a
+				JOIN web_events we
+				ON a.id = we.account_id
+				GROUP BY a.id, a.name, we.channel
+				ORDER BY a.id) t1
+		GROUP BY t1.id, t1.name
+		ORDER BY t1.id) t2
+ON t2.id = t3.id AND t2.max_chan = t3.ct
+ORDER BY t3.id, t3.ct;
