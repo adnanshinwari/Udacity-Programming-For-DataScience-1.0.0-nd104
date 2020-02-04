cd14717b369040550d546f460995d752064d3916
adnanshinwari <adnanshinwari70@gmail.com>
2020-02-04 19:57:22 +0500

    Update 4. SubQueries and Temporary Tables.sql

--- SQL Files/4. SubQueries and Temporary Tables.sql
+++ SQL Files/4. SubQueries and Temporary Tables.sql
@@ -279,3 +279,20 @@ FROM (SELECT o.account_id, AVG(o.total_amt_usd) tot_avg_amt
 		GROUP BY 1
 		HAVING AVG(o.total_amt_usd) > (SELECT AVG(o.total_amt_usd) avg_total
 										FROM orders o)) T1;
+										
+/* SubQuery using WITH */
+SELECT channel, AVG(event_count) AS avg_event_count
+FROM (SELECT DATE_TRUNC('day',occurred_at) AS day, channel, COUNT(*) AS event_count
+		FROM web_events
+	 	GROUP BY 1,2) sub
+GROUP BY 1
+ORDER BY 2 DESC;
+
+WITH events AS (SELECT DATE_TRUNC('day',occurred_at) AS day, channel, COUNT(*) AS event_count
+	FROM web_events
+	GROUP BY 1,2)
+
+SELECT channel, AVG(event_count) AS avg_event_count
+FROM events
+GROUP BY 1
+ORDER BY 2 DESC;
